b2d4b1b55329687487660b220b36bd258a81b826
Victor Guimaraes <victor.gmr@hotmail.com>
2020-08-17 11:23:19 -0300

    imported template

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,100 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('title')</title>
+
+    <!-- Scripts -->
+
+    <script src="/js/app.js"></script>
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="/css/app.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="/template_files/css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="/template_files/css/animate.css">
+
+    <link rel="stylesheet" href="/template_files/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="/template_files/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="/template_files/css/magnific-popup.css">
+
+    <link rel="stylesheet" href="/template_files/css/aos.css">
+
+    <link rel="stylesheet" href="/template_files/css/ionicons.min.css">
+
+    <link rel="stylesheet" href="/template_files/css/flaticon.css">
+    <link rel="stylesheet" href="/template_files/css/icomoon.css">
+    <link rel="stylesheet" href="/template_files/css/style.css">
+</head>
+<body>
+    <div id="app">
+    <!--<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    </!-- Left Side Of Navbar --/>
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    </!-- Right Side Of Navbar --/>
+                    <ul class="navbar-nav ml-auto">
+                        <!-/- Authentication Links --/>
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav> -->
+
+        <main>
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>
